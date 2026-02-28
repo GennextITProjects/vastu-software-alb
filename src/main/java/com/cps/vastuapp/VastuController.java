1fe0c7ccbb9cc2a54fda486964be29e46fd9d52a
atuld <atul.raj@gennextit.com>
2026-02-28 15:10:27 +0530

    feat: add MANDUKA and GOOD_ENTRIES_BAD_ZONES to square and rectangle data
    Extended the square and rectangle data configurations to include "MANDUKA" and "GOOD_ENTRIES_BAD_ZONES" options for all four sides (DOWN SIDE NORTH, LEFT SIDE NORTH, UP SIDE NORTH, RIGHT SIDE NORTH), while maintaining existing elements. This enhancement provides additional analysis capabilities for these shape types.

--- src/main/java/com/cps/vastuapp/VastuController.java
+++ src/main/java/com/cps/vastuapp/VastuController.java
@@ -1361,17 +1361,17 @@ public class VastuController {
         circleData.put("ALL", FXCollections.observableArrayList( "MARMA_POINTS", "BAD_ZONES", "GOOD_ENTRIES"));
 
         Map<String, ObservableList<String>> squareData = new HashMap<>();
-        squareData.put("DOWN SIDE NORTH", FXCollections.observableArrayList("5_ELEMENTS", "9_ZONES", "DEVTAS", "MARMA_POINTS", "ZONES"));
-        squareData.put("LEFT SIDE NORTH", FXCollections.observableArrayList("5_ELEMENTS", "9_ZONES", "DEVTAS", "MARMA_POINTS", "ZONES"));
-        squareData.put("UP SIDE NORTH", FXCollections.observableArrayList("5_ELEMENTS", "9_ZONES", "DEVTAS", "MARMA_POINTS", "ZONES"));
-        squareData.put("RIGHT SIDE NORTH", FXCollections.observableArrayList("5_ELEMENTS", "9_ZONES", "DEVTAS", "MARMA_POINTS", "ZONES"));
+        squareData.put("DOWN SIDE NORTH", FXCollections.observableArrayList("5_ELEMENTS", "9_ZONES", "DEVTAS", "MARMA_POINTS", "ZONES", "MANDUKA",  "GOOD_ENTRIES_BAD_ZONES"));
+        squareData.put("LEFT SIDE NORTH", FXCollections.observableArrayList("5_ELEMENTS", "9_ZONES", "DEVTAS", "MARMA_POINTS", "ZONES", "MANDUKA",  "GOOD_ENTRIES_BAD_ZONES"));
+        squareData.put("UP SIDE NORTH", FXCollections.observableArrayList("5_ELEMENTS", "9_ZONES", "DEVTAS", "MARMA_POINTS", "ZONES", "MANDUKA",  "GOOD_ENTRIES_BAD_ZONES"));
+        squareData.put("RIGHT SIDE NORTH", FXCollections.observableArrayList("5_ELEMENTS", "9_ZONES", "DEVTAS", "MARMA_POINTS", "ZONES", "MANDUKA" , "GOOD_ENTRIES_BAD_ZONES"));
 
 
         Map<String, ObservableList<String>> rectangleData = new HashMap<>();
-        rectangleData.put("DOWN SIDE NORTH", FXCollections.observableArrayList("5_ELEMENTS", "9_ZONES", "DEVTAS", "MARMA_POINTS", "ZONES"));
-        rectangleData.put("LEFT SIDE NORTH", FXCollections.observableArrayList("5_ELEMENTS", "9_ZONES", "DEVTAS", "MARMA_POINTS", "ZONES"));
-        rectangleData.put("UP SIDE NORTH", FXCollections.observableArrayList("5_ELEMENTS", "9_ZONES", "DEVTAS", "MARMA_POINTS", "ZONES"));
-        rectangleData.put("RIGHT SIDE NORTH", FXCollections.observableArrayList("5_ELEMENTS", "9_ZONES", "DEVTAS", "MARMA_POINTS", "ZONES"));
+        rectangleData.put("DOWN SIDE NORTH", FXCollections.observableArrayList("5_ELEMENTS", "9_ZONES", "DEVTAS", "MARMA_POINTS", "ZONES", "MANDUKA",  "GOOD_ENTRIES_BAD_ZONES"));
+        rectangleData.put("LEFT SIDE NORTH", FXCollections.observableArrayList("5_ELEMENTS", "9_ZONES", "DEVTAS", "MARMA_POINTS", "ZONES", "MANDUKA",  "GOOD_ENTRIES_BAD_ZONES"));
+        rectangleData.put("UP SIDE NORTH", FXCollections.observableArrayList("5_ELEMENTS", "9_ZONES", "DEVTAS", "MARMA_POINTS", "ZONES", "MANDUKA",  "GOOD_ENTRIES_BAD_ZONES"));
+        rectangleData.put("RIGHT SIDE NORTH", FXCollections.observableArrayList("5_ELEMENTS", "9_ZONES", "DEVTAS", "MARMA_POINTS", "ZONES", "MANDUKA",  "GOOD_ENTRIES_BAD_ZONES"));
 
         Map<String, ObservableList<String>> triangleData = new HashMap<>();
         triangleData.put("ALL", FXCollections.observableArrayList("BAD_ZONES", "DEVTAS", "GOOD_ENTRIES", "ZONES"));
